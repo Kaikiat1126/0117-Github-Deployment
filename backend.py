9cc57f6c3fccfb99fa97b544c00beb3a8ac46f36
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-01-17 04:04:41 +0800

    Fifth Commit

--- backend.py
+++ backend.py
@@ -1,11 +1,14 @@
+##动态加载html文件
+from flask_cors import CORS
 from flask import Flask, request
 
 app = Flask(__name__)
+CORS(app, resources=r'/*')
 
-@app.route('/api', methods=['get'])
-def api():
+@app.route('/html', methods=['get'])
+def getData():
     #get arguments from request
-    return "Invalid event type"
+    return '<h1>hello world</h1><script></script>'  #template flask JInjia2
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1' , port=81)
